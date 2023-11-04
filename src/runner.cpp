13400344dc32d3ad2744eadc388eb7d390509cbe
Lucas Brizola <tyfvf2@gmail.com>
2023-11-04 20:50:02 -0300

    Runner: Only 3 things remain animation, reset and limit the jump.

--- src/runner.cpp
+++ src/runner.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <vector>
+#include <random>
+#include <sstream>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_ttf.h>
 
-std::vector <SDL_Rect> CreateRect(int x, int y, int w, int h);
 bool AABB(SDL_Rect a, SDL_Rect b);
 
 int main(int argc, char* argv[]){
@@ -19,6 +20,7 @@ int main(int argc, char* argv[]){
 
     const int WINDOW_WIDTH = 800;
     const int WINDOW_HEIGHT = 400;
+    int score = 0;
     
     SDL_Window* window = SDL_CreateWindow("Runner", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
@@ -27,23 +29,29 @@ int main(int argc, char* argv[]){
     SDL_Texture* groundTex = IMG_LoadTexture(renderer, "res/ground.png");
     SDL_Texture* playerTex = IMG_LoadTexture(renderer, "res/player.png");
     SDL_Texture* snailTex = IMG_LoadTexture(renderer, "res/snail.png");
+    SDL_Texture* flyTex = IMG_LoadTexture(renderer, "res/fly.png");
+    TTF_Font* comicSans = TTF_OpenFont("res/comic.ttf", 32);
 
-    std::vector <SDL_Rect> skyRect = CreateRect(0, 0, 800, 300);
     
-    std::vector <SDL_Rect> groundRect = CreateRect(0, skyRect[1].h, 800, 100);
 
-    std::vector <SDL_Rect> playerRect = CreateRect(100, WINDOW_HEIGHT - 32, 32, 32);
-    SDL_QueryTexture(playerTex, nullptr, nullptr, &playerRect[0].w, &playerRect[0].h);
-    SDL_QueryTexture(playerTex, nullptr, nullptr, &playerRect[1].w, &playerRect[1].h);
+    SDL_Rect skyRect = {0, 0, 800, 300};
+    SDL_Rect playerRect = {100, WINDOW_HEIGHT - 32, 32, 32};
+    SDL_QueryTexture(playerTex, nullptr, nullptr, &playerRect.w, &playerRect.h);
+    SDL_Rect groundRect = {0, skyRect.h, 800, 100};
+    SDL_Rect snailRect = {WINDOW_WIDTH, skyRect.h - 32, 32, 32};
+    SDL_QueryTexture(snailTex, nullptr, nullptr, &snailRect.w, &snailRect.h);
+    SDL_Rect flyRect = {WINDOW_WIDTH, skyRect.h - 120, 32, 32};
+    SDL_QueryTexture(flyTex, nullptr, nullptr, &flyRect.w, &flyRect.h);
+    SDL_Rect fontRect = {WINDOW_WIDTH / 2, 0, 32, 32};
+    
+
     int velocity = 1;
     int speed = 5;
-
-    std::vector <SDL_Rect> snailRect = CreateRect(WINDOW_WIDTH, skyRect[1].h - 32, 32, 32);
-    SDL_QueryTexture(snailTex, nullptr, nullptr, &snailRect[0].w, &snailRect[0].h);
-    SDL_QueryTexture(snailTex, nullptr, nullptr, &snailRect[1].w, &snailRect[1].h);
+    std::vector <SDL_Rect> enemyRect = {flyRect, snailRect};
 
     SDL_Event event;
     bool gameRunning = true;
+    int enemy = rand() % 2;
 
     while(gameRunning){
         while(SDL_PollEvent(&event)){
@@ -69,26 +77,42 @@ int main(int argc, char* argv[]){
 
         }
 
-        playerRect[1].y += velocity * speed;
-        snailRect[1].x -= 5;
+        std::stringstream convert;
+        convert << score;
+        SDL_Surface* fontSurf = TTF_RenderText_Blended(comicSans, convert.str().c_str(), SDL_Color{15, 15, 15, 255});
+        SDL_Texture* fontTex = SDL_CreateTextureFromSurface(renderer, fontSurf);
+        SDL_FreeSurface(fontSurf);
+        SDL_QueryTexture(fontTex, nullptr, nullptr, &fontRect.w, &fontRect.h);
+        convert.clear();
 
-        if(AABB(playerRect[1], snailRect[1])){
+        playerRect.y += velocity * speed;
+        enemyRect[enemy].x -= 5;
+
+        if(AABB(playerRect, enemyRect[enemy])){
             std::cout << "HIT" << "\n";
         }
 
-        if(playerRect[1].y  + playerRect[1].h >= skyRect[1].h){
-            playerRect[1].y = skyRect[1].h - playerRect[1].h;
+        if(playerRect.y  + playerRect.h >= skyRect.h){
+            playerRect.y = skyRect.h - playerRect.h;
         }
 
-        if(snailRect[1].x < 0){
-            snailRect[1].x = WINDOW_WIDTH;
+        if(enemyRect[enemy].x < 0){
+            score++;
+            enemy = rand() % 2;
+            enemyRect[0].x = WINDOW_WIDTH;
+            enemyRect[1].x = WINDOW_WIDTH;
         }
 
         SDL_RenderClear(renderer);
-        SDL_RenderCopy(renderer, skyTex, &skyRect[0], &skyRect[1]);
-        SDL_RenderCopy(renderer, groundTex, &groundRect[0], &groundRect[1]);
-        SDL_RenderCopy(renderer, playerTex, &playerRect[0], &playerRect[1]);
-        SDL_RenderCopy(renderer, snailTex, &snailRect[0], &snailRect[1]);
+        SDL_RenderCopy(renderer, skyTex, nullptr, &skyRect);
+        SDL_RenderCopy(renderer, groundTex, nullptr, &groundRect);
+        SDL_RenderCopy(renderer, playerTex, nullptr, &playerRect);
+        if(enemy == 1){
+            SDL_RenderCopy(renderer, snailTex, nullptr, &enemyRect[enemy]);
+        }else{
+            SDL_RenderCopy(renderer, flyTex, nullptr, &enemyRect[enemy]);
+        }
+        SDL_RenderCopy(renderer, fontTex, nullptr, &fontRect);
         SDL_RenderPresent(renderer);
     }
 
@@ -97,26 +121,6 @@ int main(int argc, char* argv[]){
     SDL_Quit();
 
     return 0;
-}
-
-std::vector <SDL_Rect> CreateRect(int x, int y, int w, int h){
-    SDL_Rect srcRect;
-    srcRect.x = 0;
-    srcRect.y = 0;
-    srcRect.w = w;
-    srcRect.h = h;
-
-    SDL_Rect dstRect;
-    dstRect.x = x;
-    dstRect.y = y;
-    dstRect.w = w;
-    dstRect.h = h;
-
-    std::vector <SDL_Rect> rect;
-    rect.emplace_back(srcRect);
-    rect.emplace_back(dstRect);
-
-    return rect;
 }
 
 bool AABB(SDL_Rect a, SDL_Rect b){
