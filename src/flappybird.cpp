1158424fde60b43665eb2eebc5f485b27976bdf2
Lucas Brizola <tyfvf2@gmail.com>
2023-11-13 21:11:19 -0300

    FlappyBird: Added sprites and animations

--- src/flappybird.cpp
+++ src/flappybird.cpp
@@ -42,7 +42,7 @@ int main(int argc, char* argv[]){
     SDL_Window* window = SDL_CreateWindow("Flappy Bird", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
-    SDL_Texture* player_texture = IMG_LoadTexture(renderer, "res/flappybird/bird0-sheet.png");
+    SDL_Texture* player_texture = IMG_LoadTexture(renderer, "res/flappybird/bird.png");
     SDL_Texture* background_texture = IMG_LoadTexture(renderer, "res/flappybird/background.png");
     SDL_Texture* ground_texture = IMG_LoadTexture(renderer, "res/flappybird/ground.png");
     SDL_Texture* pipe_up_texture = IMG_LoadTexture(renderer, "res/flappybird/pipe_up.png");
@@ -91,8 +91,6 @@ int main(int argc, char* argv[]){
             frame_time = 0;
             player.animate();
         }
-
-        std::cout << frame_time << "\n";
 
         player.dst_rect.y += player_speed * player_velocity;
         pipe_up_rect.x -= 2;
